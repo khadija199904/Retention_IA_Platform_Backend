1744ad90fd22e4b6f70e8386d43194629dc6f431
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-12-30 20:37:06 +0100

    Ajout une table des emplpyees

---
+++ api_app/models/employee.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+
+Base = declarative_base()
+
+class EmployeeTable(Base):
+    __tablename__ = "employees"
+
+    # employeeid est la clé primaire
+    employeeid = Column(Integer, primary_key=True, index=True)
+    
+    Age = Column(Integer)
+    BusinessTravel = Column(String)
+    DailyRate = Column(Integer)
+    Department = Column(String)
+    DistanceFromHome = Column(Integer)
+    EducationField = Column(String)
+    EnvironmentSatisfaction = Column(Integer)
+    JobInvolvement = Column(Integer)
+    JobLevel = Column(Integer)
+    JobRole = Column(String)
+    JobSatisfaction = Column(Integer)
+    MaritalStatus = Column(String)
+    MonthlyIncome = Column(Integer)
+    OverTime = Column(String)
+    StockOptionLevel = Column(Integer)
+    TotalWorkingYears = Column(Integer)
+    TrainingTimesLastYear = Column(Integer)
+    WorkLifeBalance = Column(Integer)
+    YearsAtCompany = Column(Integer)
+    YearsInCurrentRole = Column(Integer)
+    YearsWithCurrManager = Column(Integer)
+    
+    # Relation vers l'historique : permet de faire employee.predictions
+    predictions = relationship("PredictionHistory", back_populates="employee")
