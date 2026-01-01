2d3fa75435959686ac24eb503834edf6258e2b5a
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2026-01-01 02:24:51 +0100

    solve problem od deploy

--- api_app/main.py
+++ api_app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .database import Base, engine
-from .routers import auth,predict,generate_plan
+from api_app.database import Base, engine
+from api_app.routers import auth,predict,generate_plan
 
 
 
